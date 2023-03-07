03ea664fd8102bc77350479927750e9490f48cf5
NEWJYH <joung5846@naver.com>
2023-03-07 15:31:47 +0900

    FrontEnd setting

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 
 const app = express();
-
 const port = 3000;
-
+// json file 입출력 설정
 app.use(express.json());
 
-console.log("Hello");
+// pug 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+// static 작업
+app.use("/public", express.static(__dirname + "/public"));
+
+// route 설정
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 app.listen(port, () => {
   console.log(`Joom app listening on port ${port}`);
+  console.log(`http://localhost:3000`);
 });
