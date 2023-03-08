740e633484d6f175682059a833e4d03675043503
NEWJYH <joung5846@naver.com>
2023-03-08 10:47:22 +0900

    소켓 메시지를 JSON형태로 변환

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,29 +1,37 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
-});
-
-socket.addEventListener("message", (message) => {
-  console.log("==========Server==========");
-  console.log(message.data);
-  console.log("==========================");
-  const li = document.createElement("li");
-  li.innerText = message.data.toString();
-  messageList.appendChild(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
 
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data.toString();
+  messageList.append(li);
+});
+
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  const message = input.value;
-  socket.send(message);
+  socket.send(makeMessage("message", input.value));
   input.value = "";
   input.focus();
+});
+
+nickForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
 });
