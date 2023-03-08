3309bc90acd974d5feb44a634643bb5ed4529682
NEWJYH <joung5846@naver.com>
2023-03-08 14:57:31 +0900

    socketio로 룸 컨셉잡기

--- Joom_socketio/src/public/js/app.js
+++ Joom_socketio/src/public/js/app.js
@@ -1,1 +1,21 @@
 const socket = io();
+
+const welcome = document.querySelector("#welcome");
+const form = welcome.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  // websocket.send와 같음
+  // 원하는 것을 emit해주면됨
+  
+  /**
+   * event 이름
+   * payload 보내고 싶은 데이터
+   * function 서버에서 호출할수 있는 함수
+   */
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+});
