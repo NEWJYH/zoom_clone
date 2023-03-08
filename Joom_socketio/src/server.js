dba4abf88cce32fad8923068f7208cbdb2d30b23
NEWJYH <joung5846@naver.com>
2023-03-08 12:47:55 +0900

    socket.io 연결

---
+++ Joom_socketio/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+
+const app = express();
+const port = 3000;
+
+// json file 입출력 설정
+app.use(express.json());
+
+// pug 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+// static 작업
+app.use("/public", express.static(__dirname + "/public"));
+
+// route 설정
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+// http server
+const httpServer = http.createServer(app);
+
+// io server
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  console.log(socket);
+});
+
+httpServer.listen(port, () => {
+  console.log(`Joom app listening on http://localhost:${port}`);
+});
