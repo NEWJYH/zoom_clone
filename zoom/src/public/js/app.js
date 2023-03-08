96959643d693bc55e0716f2a09ab63c3ac3f6ac8
NEWJYH <joung5846@naver.com>
2023-03-08 09:29:53 +0900

    WebSocket을 이용한 FrontEnd와 BackEnd socket 통신

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,1 +1,24 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("==========Server==========");
+  console.log(message.data);
+  console.log("==========================");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
+
+// let messageCount = 0;
+// setInterval(() => {
+//   socket.send(`${++messageCount}`);
+// }, 1000);
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -20,23 +20,28 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 // ws 설정
-const handleListen = () =>
-  console.log(`Joom app listening on http://localhost:${port}`);
 const server = http.createServer(app);
 // http와 webSocket 둘다 작동시키기 위함
 // WebSocket.Server({server]})를 넘기게 되면 http서버와 webSocket 서버둘다 돌릴수 있음
 const wss = new WebSocket.Server({ server });
 // http 서버를 원하지 않는다면 인자로 넘길 필요 없음
 
-
-function handleConnection(socket) {
-  console.log(socket);
-}
 // 웹소켓 이벤트
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
 
+  socket.send("Hello");
 
+  socket.on("message", (message) => {
+    console.log("==========Browser==========");
+    console.log(`${message}`);
+    console.log("===========================");
+  });
 
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+});
 
-
-server.listen(port, handleListen);
+// ws 설정
+server.listen(port, () => {
+  console.log(`Joom app listening on http://localhost:${port}`);
+});
